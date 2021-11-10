10c0f81acadf0fa33d974985dc392d4b975a0cab
acronaut1 <opruski@ryerson.ca>
2021-11-10 01:33:33 -0500

    REFACTOR for compatible index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,27 @@
 const request = require('request');
-let arg = process.argv[2];
-let url = `https://api.thecatapi.com/v1/breeds/search?q=`;
 
-const breedFile = function(callback) {
-  request(`${url}${arg}`, (err, res, body) => {
+const fetchBreedDescription = function (breedName, callback) {
+  let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+  request(url, (err, res, body) => {
     console.log(`[Response]: ${res && res.statusCode}`);
-    if (err) {
-      console.log(`error is ${err}`);
+    if (err === undefined) {
+      callback(error, null);
       return;
     }
-
+    
+    if (body === "[]") {error = `\n😿:"Missing breed not found..."`;
+      callback(error, null);
+      return;
+    }
     const data = JSON.parse(body);
-    //console.log(`[typeof-data]:`, typeof data);
-    const specimen = data[0];
-    if (specimen) {
-      callback(specimen);
-    } else {
-      console.log(`\n😿:"fail to find the breed ${data[0]}..."`);
-      return;
-    }
-  });
-};
-
-const GETdescription = (data) => {
-  console.log(`\n {[🐈-ID]: ${data.id}  [Name]: ${data.name} }:\n [Description]: ${data.description}`);
-};
-
-breedFile(GETdescription);
+    let catDesc = data[0];
+    
+    let CATdesc = () => {
+      console.log(`\n {[🐈-ID]: ${catDesc.id}  [Name]: ${catDesc.name} }:\n [Description]: ${catDesc.description}`);
+    };
+    
+    callback(null, CATdesc());
+  })
+}
+module.exports = { fetchBreedDescription };
